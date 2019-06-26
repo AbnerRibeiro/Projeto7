005772daece81c5ae156b7f2283f45bc99f82a2b
Abner Ribeiro <abnergoulart@yahoo.com.br>
2019-06-25 21:19:25 -0300

    JS ok, trabalhando no frontend

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
+// Função para gerar elementos no DOM
 function gerarPizza() {
 
 	var containerPizzas = document.querySelector("#container-pizzas");
-
 	var pizza = document.createElement("li");
-	var pizzaImg = document.createElement("img");
 
+// Adicionando classe do bootstrap inline
+	pizza.className = "list-inline-item";
+	var pizzaImg = document.createElement("img");
 	pizzaImg.setAttribute("src", "https://source.unsplash.com/100x100/?pizza"); 
 	pizzaImg.className = "borda-redonda";
 	pizza.appendChild(pizzaImg);
 	containerPizzas.appendChild(pizza);
 }
 
+// Coletando o botão
 var botao = document.querySelector("button");
 
+// Gerando pizzas no evento onclick
 botao.onclick = function(){
 	gerarPizza();
 }
